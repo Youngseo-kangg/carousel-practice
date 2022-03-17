f6c90eaea307bc0759b3df18a494695b2e5fb343
Youngseo Kang <youngseo.kangg@gmail.com>
2022-03-17 23:20:18 +0900

    oneSlider hook 작성

--- src/pages/oneSlider.js
+++ src/pages/oneSlider.js
@@ -1,30 +1,49 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import card1 from '../assets/card1.jpg';
 import card2 from '../assets/card2.png';
 import card3 from '../assets/card3.png';
+import '../styles/oneSlider.css';
 
 function OneSlider() {
-  const [currentIdx, setCurrentIdx] = useState(0);
   let cards = [card1, card2, card3];
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const changeIdx = (direction) => {
+    if (direction === '+') {
+      if (currentIdx >= cards.length - 1) {
+        setCurrentIdx(0);
+      } else {
+        setCurrentIdx(currentIdx + 1);
+      }
+    } else if (direction === '-') {
+      if (currentIdx <= 0) {
+        setCurrentIdx(cards.length - 1);
+      } else {
+        setCurrentIdx(currentIdx - 1);
+      }
+    }
+  };
+
   return (
     <article id='oneSliderWrapper'>
-      <button
-        id='oneSliderBeforeBtn'
-        onClick={() => setCurrentIdx(currentIdx--)}
-      >
-        -
-      </button>
-      <div id='oneSlider'>
-        {cards.map((card) => {
-          return <img src={card} alt='동물의 숲 아미보 카드' />;
-        })}
+      <div className='buttonWrapper'>
+        <button id='oneSliderBeforeBtn' onClick={() => changeIdx('-')}>
+          -
+        </button>
       </div>
-      <button
-        id='oneSliderAfterBtn'
-        onClick={() => setCurrentIdx(currentIdx++)}
-      >
-        +
-      </button>
+
+      <div id='oneSlider'>
+        <div id='oneSliderList'>
+          <div id='oneSlider'>
+            <img src={cards[currentIdx]} alt='동물의 숲 아미보 카드' />
+          </div>
+        </div>
+      </div>
+
+      <div className='buttonWrapper'>
+        <button id='oneSliderAfterBtn' onClick={() => changeIdx('+')}>
+          +
+        </button>
+      </div>
     </article>
   );
 }
