462a95cd93d6a3c21282ea8ca407fbc4cce70908
Youngseo Kang <youngseo.kangg@gmail.com>
2022-03-22 23:31:31 +0900

    보여지는 사진 index 관련 함수 작성

--- src/pages/multipleSlider.js
+++ src/pages/multipleSlider.js
@@ -5,7 +5,7 @@ import pizza from '../assets/pizza.jpeg';
 import salmon from '../assets/salmon.jpeg';
 import '../styles/multipleSlider.css';
 
-// 현재 창 사이즈 구하는 useWindowSize
+// * 현재 창 사이즈 구하는 useWindowSize
 function useWindowSize() {
   const [size, setSize] = useState([0, 0]);
   useLayoutEffect(() => {
@@ -20,23 +20,62 @@ function useWindowSize() {
 }
 
 function MultipleSlider() {
-  // 현재 창 사이즈에서의 슬라이드의 width 구하는 getNewItemWidth
+  // TODO : 현재 창 사이즈 관련
+  const [windowWidth, windowHeight] = useWindowSize();
+  let sliderPadding = 40;
+  // * 현재 창 사이즈에서의 슬라이드의 width 구하는 getNewItemWidth
   function getNewItemWidth() {
     // windowWidth에 0.9배 - sliderPadding*2 빼준 길이로 itemWidth 변수 만들어주기
     let itemWidth = windowWidth * 0.9 - sliderPadding * 2;
     itemWidth = itemWidth > 1060 ? 1060 : itemWidth;
     return itemWidth;
   }
-
-  let cards = [berries, cake, pizza, salmon];
-  let sliderPadding = 40;
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [windowWidth, windowHeight] = useWindowSize();
-  // currentIndex 변경하는 함수 handleSwipe
-  const handleSwipe = (index) => {
-    setCurrentIndex((currentIndex) => currentIndex + index);
-  };
   const newItemWidth = getNewItemWidth();
+
+  // TODO : 보여질 카드 관련
+  let cards = [berries, cake, pizza, salmon];
+  const endData = 2; // 양 끝에 추가될 데이터 수 endData
+  // * 복제한 슬라이드까지 포함 하도록 만드는 함수 setCards
+  const setCards = () => {
+    let addedFront = [];
+    let addedRear = [];
+    let index = 0;
+    while (index < endData) {
+      // 앞부분에는 cards에 가장 마지막 부분 복제해서 넣어주기
+      addedFront.unshift(cards[cards.length - 1 - (index % cards.length)]); // 3(berries),2(cake)번째 추가
+      // 뒷부분에는 cards에 가장 앞부분 복제해서 넣어주기
+      addedRear.push(cards[index % cards.length]); // 0(pizza),1(salmon)번째 추가
+      index++;
+    }
+    return [...addedFront, ...cards, ...addedRear];
+  };
+  let modCards = setCards();
+
+  // TODO : index 재조정 관련
+  // * 보여지는 card의 실제 index를 구하는 함수 getItemIndex
+  function getItemIndex(index) {
+    index -= endData; // cards의 일부분이 복제된 상황이라 추가한 endData만큼 빼서 실제 index로 보기
+    if (index < 0) index += cards.length;
+    // 0보다 작으면 cards.length만큼 더해서 복구
+    else if (index >= cards.length) index -= cards.length; // cards.length보다 크면 cards.length만큼 뺴서 복구
+    return index;
+  }
+
+  // TODO : 슬라이드 이동 관련
+  const [currentIndex, setCurrentIndex] = useState(0);
+  // * 몇번째 슬라이드를 보여줄 지 currentIndex 변경하는 함수 handleSwipe
+  const handleSwipe = (direction) => {
+    // setCurrentIndex((currentIndex) => currentIndex + index);
+    handleCarousel(currentIndex + direction);
+  };
+  // * handleSwipe를 도와 currentIndex 범위 조정하는 함수 handleCarousel
+  const handleCarousel = (index) => {
+    // 들어온 수가 0보다 작다면 가장 마지막 slide index로
+    if (index < 0) index = cards.length - 1;
+    // 들어온 수가 cards.length와 크거나 같다면 가장 첫번째 slide index로
+    else if (index >= cards.length) index = 0;
+    setCurrentIndex(index);
+  };
 
   return (
     <div id='sliderArea'>
@@ -60,14 +99,14 @@ function MultipleSlider() {
               }%)`,
             }}
           >
-            {cards.map((el, idx) => {
+            {modCards.map((el, idx) => {
+              const itemIndex = getItemIndex(idx);
               return (
-                <div className='multipleSliderItem'>
+                <div key={idx} className='multipleSliderItem'>
                   <img
                     style={{ width: newItemWidth || 'auto' }}
-                    key={idx}
                     src={el}
-                    alt='동물의 숲 아미보 카드'
+                    alt={`실제 인덱스:${idx}, 재조정한 인덱스: ${itemIndex}`}
                   />
                 </div>
               );
